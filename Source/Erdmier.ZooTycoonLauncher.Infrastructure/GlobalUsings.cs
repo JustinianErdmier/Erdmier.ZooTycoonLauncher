c64a72a9778f93e1b6546ffb88550117133469a2
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2026-05-27 07:26:12 -0500

    feat(✨): add Infrastructure project with AppStorageLocations

---
+++ Source/Erdmier.ZooTycoonLauncher.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using System.IO.Abstractions;
+global using Erdmier.ZooTycoonLauncher.Application.Common.Abstractions;
+global using Erdmier.ZooTycoonLauncher.Domain.Installations;
+global using Erdmier.ZooTycoonLauncher.Domain.IniSnapshots;
+global using Erdmier.ZooTycoonLauncher.Domain.Settings;
+global using JetBrains.Annotations;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using Serilog;

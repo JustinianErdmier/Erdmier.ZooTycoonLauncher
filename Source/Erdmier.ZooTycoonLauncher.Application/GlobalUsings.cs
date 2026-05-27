d7fecc8c9fc23f18188cde0ec1c2091df6166a63
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2026-05-27 07:22:50 -0500

    feat(✨): add Application project with foundational abstractions

---
+++ Source/Erdmier.ZooTycoonLauncher.Application/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System.IO.Abstractions;
+global using ErrorOr;
+global using Erdmier.ZooTycoonLauncher.Domain.Installations;
+global using Erdmier.ZooTycoonLauncher.Domain.Settings;
+global using Erdmier.ZooTycoonLauncher.Domain.IniSnapshots;
+global using JetBrains.Annotations;
+global using Mediator;
+global using Microsoft.Extensions.DependencyInjection;

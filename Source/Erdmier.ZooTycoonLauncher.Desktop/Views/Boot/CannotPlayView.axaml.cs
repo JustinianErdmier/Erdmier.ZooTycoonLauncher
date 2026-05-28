5c85503badfb8ff29d0c68fb9ec07697b63e04fb
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2026-05-28 15:40:15 -0500

    feat(✨): add Boot state views

---
+++ Source/Erdmier.ZooTycoonLauncher.Desktop/Views/Boot/CannotPlayView.axaml.cs
@@ -0,0 +1,8 @@
+namespace Erdmier.ZooTycoonLauncher.Desktop.Views.Boot;
+
+/// <summary>View for the CannotPlay state.</summary>
+public sealed partial class CannotPlayView : UserControl
+{
+    /// <summary>Initialises a new instance.</summary>
+    public CannotPlayView() => AvaloniaXamlLoader.Load(this);
+}
---
+++ Source/Erdmier.ZooTycoonLauncher.Desktop/Views/Boot/OpenGameInstallationView.axaml.cs
@@ -0,0 +1,8 @@
+namespace Erdmier.ZooTycoonLauncher.Desktop.Views.Boot;
+
+/// <summary>View for the OpenGameInstallation state.</summary>
+public sealed partial class OpenGameInstallationView : UserControl
+{
+    /// <summary>Initialises a new instance.</summary>
+    public OpenGameInstallationView() => AvaloniaXamlLoader.Load(this);
+}
